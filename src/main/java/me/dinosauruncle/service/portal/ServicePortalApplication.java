da7ef24c2a9ee34599a515b8bcf50563e7ef4ab5
ion_chris <chris@i-on.net>
2020-04-01 12:19:57 +0900

    클래스 이름 변경

--- src/main/java/me/dinosauruncle/service/portal/ServicePortalApplication.java
+++ src/main/java/me/dinosauruncle/service/portal/ServicePortalApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-class AccountApplication {
+class ServicePortalApplication {
     public static void main(String[] args) {
-        SpringApplication.run(AccountApplication.class, args);
+        SpringApplication.run(ServicePortalApplication.class, args);
     }
 }
